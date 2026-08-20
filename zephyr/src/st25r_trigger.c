29acfb5e24f5280d793eb4a255b126d75b81c49d
QORAAL-LAPTOP\natie <natie@navaro.nl>
2026-08-20 17:07:06 +0200

    fix: run ST25R trigger thread preemptively

--- zephyr/src/st25r_trigger.c
+++ zephyr/src/st25r_trigger.c
@@ -98,7 +98,7 @@ int st25r_init_interrupt(const struct device *dev)
 	k_thread_create(&st25r->thread, st25r->thread_stack,
                     CONFIG_ST25R_THREAD_STACK_SIZE,
 		       (k_thread_entry_t)st25r_thread, st25r,
-		       0, NULL, K_PRIO_COOP(CONFIG_ST25R_THREAD_PRIORITY),
+		       0, NULL, K_PRIO_PREEMPT(CONFIG_ST25R_THREAD_PRIORITY),
 		       0, K_NO_WAIT);
 #elif defined(CONFIG_ST25R_TRIGGER_GLOBAL_THREAD)
 	st25r->work.handler = st25r_work_cb;
